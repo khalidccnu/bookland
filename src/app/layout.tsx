3bf0bfd710df083583d18cbe2a50bde56b6064b5
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-08-30 11:13:12 +0600

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Josefin_Sans } from "next/font/google";
+import brandIcon from "./fi-bookland.svg";
+import "./globals.css";
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-josefinSans",
+});
+
+export const metadata: Metadata = {
+  title: "BookLand",
+  description:
+    "BookLand is the largest online bookstore in Bangladesh. Buy books at lowest price.",
+  icons: {
+    shortcut: brandIcon.src,
+  },
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefinSans.variable} font-josefinSans flex flex-col min-h-screen`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
